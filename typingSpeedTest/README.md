6e613e7ecc6c70e39f42c359d0fa383f03b6222d
adnan <adnan@habbat.ml>
2018-11-24 00:38:20 +0100

    Added some functionality and a README file

---
+++ typingSpeedTest/README.md
@@ -0,0 +1,5 @@
+typingSpeedTest
+
+In this project I tried to implement event listeners to make a fun to use speed typing tester, this is the first version of this project I'll add some functionality later such as word per minute, miss typed words and a count down timer to make it more challenging.
+
+Version 1.0
--- typingSpeedTest/script.js
+++ typingSpeedTest/script.js
@@ -1,19 +1,28 @@
+// Version 1.0
 const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
 // const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var textExamples = ['',
-                    'that',
-                    'today'
-                    ]
-var item = textExamples[Math.floor(Math.random()*textExamples.length)]
+var textExamples = [
+                    'this is a simple paragraph that is meant to be nice and easy to type which is why there will \
+                    be commas no periods or any capital letters so i guess this means that it cannot really be considered\
+                    paragraph but just a series of run on sentences.',
+                    'The practice of writing paragraphs is essential to good writing. Paragraphs help to break up large chunks of text\
+                    and makes the content easier for readers to digest.',
+                    'A language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.\
+                    Alongside HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web.',
+                    'Python is an interpreted high-level programming language for general-purpose programming. Created by Guido van Rossum\
+                    and first released in 1991, Python has a design philosophy that emphasizes code readability, notably using significant\
+                    whitespace. It provides constructs that enable clear programming on both small and large scales.',
+                    'The bikers rode down the long and narrow path to reach the city park. When they reached a good spot to rest,\
+                    they began to look for signs of spring. The sun was bright, and a lot of bright red and blue blooms proved to all\
+                    that warm spring days were the very best.'
+                    ];
+
+var item = textExamples[Math.floor(Math.random()*textExamples.length)];
 const originText = document.querySelector("#origin-text p").innerHTML = item;
-
-
-console.log(document.querySelector("#origin-text p").innerHTML) 
-
 
 var timer = [0,0,0,0];
 var interval;
