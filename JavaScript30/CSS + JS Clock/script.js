03fde3152a3cb60099f0e09bfbe0dc5ab54ff837
adnan <adnan@habbat.ml>
2018-11-26 18:24:01 +0100

    Initial commit

--- JavaScript30/CSS + JS Clock/script.js
+++ JavaScript30/CSS + JS Clock/script.js
@@ -4,17 +4,27 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate(){
     const now = new Date();
-
     const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    if(seconds==0){
+        secondHand.style.transitionDuration = '0s';
+        minuteHand.style.transitionDuration = '0s';
+        hourHand.style.transitionDuration = '0s';
+    } else {
+        secondHand.style.transitionDuration = '0.05s';
+        minuteHand.style.transitionDuration = '0.05s';
+        hourHand.style.transitionDuration = '0.05s';
+    }
     const secondsDegrees = ((seconds/60)*360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const minutes = now.getMinutes();
     const minutesDegrees = ((minutes/60)*360 + 90);
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-    const hours = now.getHours();
     const hoursDegrees = ((hours/12)*360 +90);
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 setInterval(setDate, 1000);
+setDate();
