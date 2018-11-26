dd9deb5a8fdb741502a2c000d6d851f4d3fbe8fe
adnan <adnan@habbat.ml>
2018-11-26 15:45:40 +0100

    Initial commit

--- JavaScript30/JavaScript Drum Kit/script.js
+++ JavaScript30/JavaScript Drum Kit/script.js
@@ -1,3 +1,19 @@
-window.addEventListener('keydown', function(e) {
-    this.console.log(e.keyCode);
-});
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip if it is not a transform
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; // stop the function from running
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
